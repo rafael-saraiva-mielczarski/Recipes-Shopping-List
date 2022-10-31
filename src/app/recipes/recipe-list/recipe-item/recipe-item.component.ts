a5dfa3b50ca9a4ab119f12936622a44a0534a1e5
Rafael Saraiva Mielczarski <Rafael.Mielczarski@ibm.com>
2022-10-31 15:21:20 -0300

    turned css files into scss

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -6,7 +6,7 @@ import { RecipeService } from '../../recipe.service';
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.css']
+  styleUrls: ['./recipe-item.component.scss']
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
--- src/app/shared/loading-spinner/loading-spinner.component.ts
+++ src/app/shared/loading-spinner/loading-spinner.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-loading-spinner',
     template: '<div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>',
-    styleUrls: ['./loading-spinner.component.css']
+    styleUrls: ['./loading-spinner.component.scss']
 })
 
 export class LoadingSpinnerComponent {
